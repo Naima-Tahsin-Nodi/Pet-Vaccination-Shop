5bf0dc629a2ffc95e7eff1893db6859f9eebe909
Naima Tahsin Nodi <99125539+Naima-Tahsin-Nodi@users.noreply.github.com>
2022-04-30 22:28:46 +0600

    Update InventoryTest.php

--- tests/InventoryTest.php
+++ tests/InventoryTest.php
@@ -13,8 +13,12 @@ class InventoryTest extends \PHPUnit\Framework\TestCase
 
 
         $mockProductsArray = [
-            ['id' => 1, 'name' => 'Acme Radio Knobs'],
-            ['id' => 2, 'name' => 'Apple iPhone'],
+            ['id' => 1, 'name' => 'Parvo virous'],
+            ['id' => 2, 'name' => 'Cevac'],
+            ['id' => 3, 'name' => 'Distemper'],
+            ['id' => 4, 'name' => 'frcpv'],
+            ['id' => 5, 'name' => 'Rabies'],
+            ['id' => 6, 'name' => 'Hepatites'],
         ];
 
         $mockRepo->expects($this->exactly(1))->method('fetchProducts')->willReturn($mockProductsArray);
@@ -24,8 +28,12 @@ class InventoryTest extends \PHPUnit\Framework\TestCase
         $inventory->setProducts();
 
         // Make assertions
-        $this->assertEquals('Acme Radio Knobs', $inventory->getProducts()[0]['name']);
-        $this->assertEquals('Apple iPhone', $inventory->getProducts()[1]['name']);
+        $this->assertEquals('Parvo virous', $inventory->getProducts()[0]['name']);
+        $this->assertEquals('Cevac', $inventory->getProducts()[1]['name']);
+        $this->assertEquals('Distemper', $inventory->getProducts()[2]['name']);
+        $this->assertEquals('frcpv', $inventory->getProducts()[3]['name']);
+        $this->assertEquals('Rabies', $inventory->getProducts()[4]['name']);
+        $this->assertEquals('Hepatites', $inventory->getProducts()[5]['name']);
     }
 
 
